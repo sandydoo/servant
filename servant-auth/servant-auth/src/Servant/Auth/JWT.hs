bf5fc674000c30eeb14a65a6981d4f181fc7e7f9
Sander <hey@sandydoo.me>
2023-12-02 18:07:06 +0000

    wip: add support from additional JWT claims

--- servant-auth/servant-auth/src/Servant/Auth/JWT.hs
+++ servant-auth/servant-auth/src/Servant/Auth/JWT.hs
@@ -2,39 +2,73 @@
 
 module Servant.Auth.JWT where
 
-import           Control.Lens         ((^.))
-import qualified Crypto.JWT           as Jose
-import           Data.Aeson           (FromJSON, Result (..), ToJSON, fromJSON,
-                                       toJSON)
-#if MIN_VERSION_aeson(2,0,0)                                                                                    
-import qualified Data.Map as KM                                                                        
-#else                                                                                                           
-import qualified Data.HashMap.Strict as KM                                                                      
+import qualified Crypto.JWT as Jose
+import Data.Aeson
+  ( FromJSON,
+    Result (..),
+    ToJSON,
+    Value (..),
+    fromJSON,
+    parseJSON,
+    toJSON,
+    withObject,
+  )
+import Data.Aeson.KeyMap as KM
+import qualified Data.Aeson.Key as Key
+import qualified Data.Text as T
+import qualified Data.Map.Strict as M
+import qualified Data.Set as S
+
+data ClaimsSet = ClaimsSet {jwtClaims :: Jose.ClaimsSet, jwtUnregisteredClaims :: M.Map T.Text Value }
+
+instance Jose.HasClaimsSet ClaimsSet where
+  claimsSet f s = fmap (\a' -> s {jwtClaims = a'}) (f $ jwtClaims s)
+
+instance FromJSON ClaimsSet where
+  parseJSON = withObject "ClaimsSet" $ \o ->
+    ClaimsSet <$> parseJSON (Object o) <*> pure (filterUnregistered . fromKeyMap $ o)
+    where
+      filterUnregistered :: M.Map T.Text Value -> M.Map T.Text Value
+      filterUnregistered m =
+#if MIN_VERSION_containers(0,5,8)
+        m `M.withoutKeys` registeredClaims
+#else
+        m `M.difference` M.fromSet (const ()) registeredClaims
 #endif
+      registeredClaims :: S.Set T.Text
+      registeredClaims = S.fromDistinctAscList
+        [ "aud"
+        , "exp"
+        , "iat"
+        , "iss"
+        , "jti"
+        , "nbf"
+        , "sub"
+        ]
 
-import qualified Data.Text            as T
+instance ToJSON ClaimsSet where
+  toJSON (ClaimsSet cs d) = case toJSON cs of
+    Object o -> Object (o <> toKeyMap d)
+    _ -> error "impossible"
 
-
--- This should probably also be from ClaimSet
---
--- | How to decode data from a JWT.
---
--- The default implementation assumes the data is stored in the unregistered
--- @dat@ claim, and uses the @FromJSON@ instance to decode value from there.
 class FromJWT a where
-  decodeJWT :: Jose.ClaimsSet -> Either T.Text a
-  default decodeJWT :: FromJSON a => Jose.ClaimsSet -> Either T.Text a
-  decodeJWT m = case KM.lookup "dat" (m ^. Jose.unregisteredClaims) of
-    Nothing -> Left "Missing 'dat' claim"
-    Just v  -> case fromJSON v of
-      Error e -> Left $ T.pack e
-      Success a -> Right a
+  decodeJWT :: (FromJSON a) => ClaimsSet -> Either T.Text a
+  default decodeJWT :: FromJSON a => ClaimsSet -> Either T.Text a
+  decodeJWT m = 
+    case M.lookup "dat" (jwtUnregisteredClaims m) of
+      Nothing -> Left "Missing dat claim"
+      Just v -> case fromJSON v of
+        Error e -> Left $ T.pack e
+        Success a -> Right a
 
--- | How to encode data from a JWT.
---
--- The default implementation stores data in the unregistered @dat@ claim, and
--- uses the type's @ToJSON@ instance to encode the data.
 class ToJWT a where
-  encodeJWT :: a -> Jose.ClaimsSet
-  default encodeJWT :: ToJSON a => a -> Jose.ClaimsSet
-  encodeJWT a = Jose.addClaim "dat" (toJSON a) Jose.emptyClaimsSet
+  encodeJWT :: (ToJSON a) => a -> ClaimsSet
+  default encodeJWT :: (ToJSON a) => a -> ClaimsSet
+  encodeJWT a =
+    ClaimsSet {jwtClaims = Jose.emptyClaimsSet, jwtUnregisteredClaims = M.fromList [("dat", toJSON a)]}
+
+fromKeyMap :: KM.KeyMap Value -> M.Map T.Text Value
+fromKeyMap = M.mapKeysMonotonic Key.toText . KM.toMap
+
+toKeyMap :: M.Map T.Text Value -> KM.KeyMap Value
+toKeyMap = KM.fromMap . M.mapKeysMonotonic Key.fromText
